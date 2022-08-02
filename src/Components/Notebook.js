d2a6284bc981f1524af01f3dcb995647ac7b98b6
Ndricim Jahaj <jahajndricim@gmail.com>
2022-08-02 20:39:24 +0200

    Set up notebook page

---
+++ src/Components/Notebook.js
@@ -0,0 +1,65 @@
+import "./notebook.scss";
+
+export default function Notebook() {
+  return (
+    <>
+      <div className="notebook">
+        <div className="notebook_sidebar">
+        <div className="notebook_sidebar_searchBar">
+              <form action="">
+                <input type="search" placeholder="Search for a note" />
+                <button>Search</button>
+              </form>
+              <h3>All Notes</h3>
+            </div>
+          <div className="notebook_sidebar_fixed">
+            <h1>Notes</h1>
+            <h1>Notes</h1>
+            <h1>Notes</h1>
+            <h1>Notes</h1>
+            <h1>Notes</h1>
+            <h1>Notes</h1>
+            <h1>Notes</h1>
+            <h1>Notes</h1>
+            <h1>Notes</h1>
+            <h1>Notes</h1>
+            <h1>Notes</h1>
+            <h1>Notes</h1>
+            <h1>Notes</h1>
+            <h1>Notes</h1>
+          </div>
+        </div>
+        <div className="notebook_notes">
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+            <h3>Notes</h3>
+        </div>
+      </div>
+    </>
+  );
+}
