d0992d9f9893c6f07b0b2e3c74671515060ea830
Ndricim Jahaj <jahajndricim@gmail.com>
2022-08-02 20:47:49 +0200

    Set up notebook page

--- src/Components/Notebook.js
+++ src/Components/Notebook.js
@@ -1,6 +1,10 @@
 import "./notebook.scss";
 
 export default function Notebook() {
+
+    const notes = ['Note1','Note1','Note1','Note1','Note1','Note1','Note1','Note1','Note1','Note1','Notes','Notes','Notes','Notes','Notes','Notes','Notes','Notes','Notes','Notes','Notes','Notes','Notes','Notes','Notes' ]
+    const allNotes = notes.map((note, i)=> <h1 key={i}>{note}</h1>)
+
   return (
     <>
       <div className="notebook">
@@ -13,51 +17,11 @@ export default function Notebook() {
               <h3>All Notes</h3>
             </div>
           <div className="notebook_sidebar_fixed">
-            <h1>Notes</h1>
-            <h1>Notes</h1>
-            <h1>Notes</h1>
-            <h1>Notes</h1>
-            <h1>Notes</h1>
-            <h1>Notes</h1>
-            <h1>Notes</h1>
-            <h1>Notes</h1>
-            <h1>Notes</h1>
-            <h1>Notes</h1>
-            <h1>Notes</h1>
-            <h1>Notes</h1>
-            <h1>Notes</h1>
-            <h1>Notes</h1>
+            {allNotes}
           </div>
         </div>
         <div className="notebook_notes">
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
-            <h3>Notes</h3>
+          
         </div>
       </div>
     </>
